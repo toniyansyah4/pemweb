4a1c1fc327597eaaacd0fd8a06c841e09bb99a5a
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2021-01-10 17:32:38 +0700

    update page login and add styling

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,6 +1,12 @@
 const username = document.querySelector(".username");
 const password = document.querySelector(".password");
 const signIn = document.querySelector(".sign-in");
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
 
 signIn.addEventListener("click", function (e) {
   e.preventDefault();
